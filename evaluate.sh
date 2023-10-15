6a3b99d9e61be1eb1fab74ef4db62117ed592199
ohashi56225 <ohashi.atsumoto.c0@s.mail.nagoya-u.ac.jp>
2023-10-15 13:40:22 +0900

    Run gpt4-fs-e2e

--- evaluate.sh
+++ evaluate.sh
@@ -1,4 +1,4 @@
 python evaluate.py \
     --dataset_dpath "dataset/JMultiWOZ_1.0" \
-    --inference_output_dpath "output/t5-large-bs32-ep5-olen512" \
+    --inference_output_dpath "output/gpt4-zs-olen256" \
     --task_name "e2e"
